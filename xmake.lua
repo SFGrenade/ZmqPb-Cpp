5fbd5b8341468ab4fcadab089957320521e8ebef
SFGrenade <25555417+SFGrenade@users.noreply.github.com>
2024-02-18 18:05:44 +0100

    add confs clean up xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,61 +1,44 @@
-add_rules("mode.debug", "mode.release")
-add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode"})
+add_rules( "mode.debug", "mode.release" )
+add_rules( "plugin.compile_commands.autoupdate", { outputdir = ".vscode" } )
 
-set_project("ZmqPb")
+set_project( "ZmqPb" )
 
-if is_plat("windows") then
-    set_languages("cxxlatest")
+if is_plat( "windows" ) then
+    set_languages( "cxxlatest" )
 
-    -- bruh
-    add_defines("_WIN32_WINNT=0x0601")
+    add_cxflags( "/Zc:__cplusplus" )
+    add_cxflags( "/Zc:preprocessor" )
 
-    add_cxflags("/Zc:__cplusplus")
-    add_cxflags("/Zc:preprocessor")
+    add_cxflags( "/permissive-" )
 
-    add_cxflags("/permissive-")
+    set_runtimes(is_mode("debug") and "MDd" or "MD")
 else
-    set_languages("cxx20")
+    set_languages( "cxx20" )
 end
 
-add_requires("cppzmq")
-add_requires("protobuf-cpp")
+set_warnings( "allextra" )
 
-set_warnings("allextra")
-
-if is_mode("debug") then
-    add_defines("DEBUG")
-
-    set_symbols("debug")
-    if is_plat("windows") then
-        add_cxflags("/Zi")
-        add_cxflags("/ZI")
-    else
-    end
-
-    set_optimize("fastest")
+if is_mode( "debug" ) then
+    add_defines( "DEBUG" )
+end
+if is_mode( "release" ) then
+    add_defines( "NDEBUG" )
 end
 
-if is_mode("release") then
-    add_defines("NDEBUG")
+add_requireconfs( "*", { configs = { shared = false } } )
 
-    if is_plat("windows") then
-        add_cxflags("/Qpar")
-    else
-        add_cxflags("-fomit-frame-pointer")
-    end
+add_requires( "cppzmq" )
+add_requires( "protobuf-cpp" )
 
-    set_optimize("fastest")
-end
+target( "ZmqPb" )
+    set_kind( "static" )
 
-target("ZmqPb")
-    set_kind("static")
+    add_packages( "cppzmq" )
+    add_packages( "protobuf-cpp", { public = false } )
 
-    add_packages("cppzmq")
-    add_packages("protobuf-cpp")
+    add_rules( "protobuf.cpp" )
 
-    add_rules("protobuf.cpp")
-
-    add_includedirs("include")
-    add_headerfiles("include/(zmqPb/*.hpp)")
-    add_files("proto/zmqPb/*.proto")
-    add_files("src/*.cpp")
+    add_includedirs( "include" )
+    add_headerfiles( "include/(zmqPb/*.hpp)" )
+    add_files( "proto/zmqPb/*.proto", { proto_public = false } )
+    add_files( "src/*.cpp" )
